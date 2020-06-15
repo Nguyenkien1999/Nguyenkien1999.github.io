a38db621ee0e689148c0fed219f4631cdde96c87
Nguyen Kien <nchangtrai@gmail.com>
2020-06-15 21:20:16 +0700

    kien

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-//const io = require('socket.io')(process.env.PORT || 3000);
-const io = require('socket.io')(3000);
+const io = require('socket.io')(process.env.PORT || 3000);
+//const io = require('socket.io')(3000);
 
 const arrUserInfo = [];
 
